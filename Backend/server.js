f04c474bb3f2e91ed68dd80baadc35ee9b7ccd3a
vyshnavi <vishuchowdary20@gmail.com>
2025-10-01 12:36:05 +0530

    mysql connection setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,24 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import sequelize from "./config/database.js";
+import User from "./models/user.js";
+import Item from "./models/Item.js";
 
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-//test route
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Lost & Found Backend is running");
+  res.send("Lost & Found Backend Running with MySQL");
 });
 
+// Sync models
+sequelize.sync({ alter: true }).then(() => {
+  console.log("Database & tables synced");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
