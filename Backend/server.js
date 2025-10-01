3a13871ae2f8e6ba63b4fb41e14406c10da46426
vyshnavi <vishuchowdary20@gmail.com>
2025-10-01 12:03:10 +0530

    test route

--- Backend/server.js
+++ Backend/server.js
@@ -11,3 +11,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+//test route
+
+app.get("/", (req, res) => {
+  res.send("Lost & Found Backend is running");
+});
