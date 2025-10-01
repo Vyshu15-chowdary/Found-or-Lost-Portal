4fff0fd7eabbf1f450c8a887826f8145be72a3c6
vyshnavi <vishuchowdary20@gmail.com>
2025-10-01 12:42:33 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,23 +2,23 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import sequelize from "./config/database.js";
-import User from "./models/user.js";
-import Item from "./models/Item.js";
+import authRoutes from "./routes/auth.js";
+import itemRoutes from "./routes/items.js";
 
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => res.send("Lost & Found Backend Running"));
 
-app.get("/", (req, res) => {
-  res.send("Lost & Found Backend Running with MySQL");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
 
-// Sync models
 sequelize.sync({ alter: true }).then(() => {
   console.log("Database & tables synced");
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
