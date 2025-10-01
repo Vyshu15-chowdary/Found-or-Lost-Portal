e1979a7b172162f835eaffa986b115ae90e5f0bf
vyshnavi <vishuchowdary20@gmail.com>
2025-10-01 12:02:13 +0530

    middleware used

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
