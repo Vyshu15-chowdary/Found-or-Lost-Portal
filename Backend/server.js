9399ff873c90508b48a7d422cba0b94b4cc13e9c
vyshnavi <vishuchowdary20@gmail.com>
2025-10-01 12:04:48 +0530

    Mongodb connection

--- Backend/server.js
+++ Backend/server.js
@@ -17,3 +17,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Lost & Found Backend is running");
 });
+
+//mongodb connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log("MongoDB connection error:", err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
