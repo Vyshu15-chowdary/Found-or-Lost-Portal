5a88c25f9da8ce8c82d1fa810ceb03decf75433f
vyshnavi <vishuchowdary20@gmail.com>
2025-10-03 17:58:49 +0530

    overall upgraded

--- Backend/routes/itemRoutes.js
+++ Backend/routes/itemRoutes.js
@@ -1,23 +1,11 @@
 import express from "express";
-import {
-  createItem,
-  getAllItems,
-  getItemById,
-  deleteItem
-} from "../controllers/itemController.js";
+import { createItem, getAllItems, deleteItem } from "../controllers/itemController.js";
 
 const router = express.Router();
 
-// Create a new item
+// Routes
 router.post("/", createItem);
-
-// Get all items
 router.get("/", getAllItems);
-
-// Get a single item by ID
-router.get("/:id", getItemById);
-
-// Delete an item by ID
 router.delete("/:id", deleteItem);
 
 export default router;
