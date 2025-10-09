8351c289d913024626189c554d85004ea8d9c426
vyshnavi <vishuchowdary20@gmail.com>
2025-10-09 22:18:29 +0530

    Title name updated

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
           backgroundImage: "url('/Necn1.jpg')",
          }} >
      <h1 className="text-6xl font-extrabold mb-6 bg-gradient-to-r from-white to-blue-400 bg-clip-text text-transparent drop-shadow-[0_3px_10px_rgba(0,0,0,0.7)]">
-  Lost & Found Portal
+  Lost & Found Management System
 </h1>
 
 
